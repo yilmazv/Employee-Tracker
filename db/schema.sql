5b4bab7b09ef638855aedff7dd14e949c66e48ef
Vehbi Yilmaz <yilmazvtc@gmail.com>
2022-04-19 17:07:35 -0700

    adding in schema.sql and seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL 
+);
+
+
+CREATE TABLE roles (
+  role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary decimal(9,2) NOT NULL,
+  dept_id INT NOT NULL 
+);
+
+
+CREATE TABLE department (
+  dept_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL,
+);
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Nancie', 'Harrington', 1, 1),
+       ('Judith', 'Krueger', 2, 2),
+       ('Cayson', 'Lim', 3, 3),
+       ('Aliza', 'Cottrell', 4, 4),
+       ('Beatrix', 'Olsen', 5, 5),
+       ('Mark', 'Jones', 6, 6),
+
+INSERT INTO role (title, salary, dept_id)
+VALUES ('Project Manager', 60000, 1),
+       ('Project Coordinator', 66000, 3),
+       ('Front End Dev', 70000, 2),
+       ('Back End Dev', 80000, 2),
+       ('Full Stack Dev', 100000, 2),
+       ('Lawyer', 200000, 4),
+
+
+
+INSERT INTO role (dept_name)
+VALUES ('Human Resources'),
+       ('Softwaree'),
+       ('Marketing'),
+       ('Legal'),
